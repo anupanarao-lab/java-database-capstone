98c1dd5251fa586145e2a0f95fe460d4f567898a
anupanarao-lab <anupanarao@gmail.com>
2025-12-26 20:45:48 +0530

    Create PatientRepository.java

---
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.project.back_end.repositories;
+
+import com.project.back_end.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+    // Custom query methods (optional)
+    Patient findByUserId(int userId);
+}
